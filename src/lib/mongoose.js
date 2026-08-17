5d0d24a807c0cce32e44fe56b87d9b428942346b
Developerindia2026 <developmentindia2026@gmail.com>
2026-08-17 13:06:56 -0500

    ok

--- src/lib/mongoose.js
+++ src/lib/mongoose.js
@@ -1,6 +1,13 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
-export default async function ConnectDb() {
-  await mongoose.connect(process.env.MONGODB_URI);
-  console.log("DATABASE CONNECTED WIH WORLKFLOW-OS");
+export default async function connectDB() {
+  const mongoURI = process.env.MONGODB_URI;
+
+  if (!mongoURI) {
+    throw new Error("MONGODB_URI is not defined");
+  }
+
+  await mongoose.connect(mongoURI);
+
+  console.log("DATABASE CONNECTED WITH WORKFLOW-OS");
 }
