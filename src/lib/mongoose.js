4f3010a37710829b9df704249cbb089baf6037c0
Developerindia2026 <developmentindia2026@gmail.com>
2026-08-17 12:57:35 -0500

    connect global mongosb

--- src/lib/mongoose.js
+++ src/lib/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
 export default async function ConnectDb() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/workflow");
+  await mongoose.connect(process.env.MONGODB_URI);
   console.log("DATABASE CONNECTED WIH WORLKFLOW-OS");
 }
